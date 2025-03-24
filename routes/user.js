407441bebc4be9dcc175bd231e38f930478989aa
marisiromanillos <marisiromanillos@gmail.com>
2025-03-24 09:27:41 +0000

    login route

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,20 @@ const sha256 = require("sha256");
 const { addUser } = require("../mysql/queries");
 const e = require("express");
 
+// login user
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  // hash the pasword
+  const sha256Password = sha256(password);
+  // compare the hash version to the stored the one
+  try {
+    const results = await asyncMySQL(checkUserCreds(email, sha256Password));
+    res.status(202).send(results);
+  } catch (e) {}
+  console.log(e);
+});
+
+// add user
 router.post("/user", async (req, res) => {
   const { name, email, password } = req.body;
   console.log("Received data:", { name, email, password });
